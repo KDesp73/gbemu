80f94b101a4ca9b69838441fb8ec3a4c712c5cd0
Konstantinos Despoinidis <despoinidisk@gmail.com>
2026-07-29 11:42:12 +0300

    feat: gb-test-roms submodule

--- src/main.c
+++ src/main.c
@@ -3,9 +3,13 @@
 
 int main(){
     CPU cpu = {0};
-    cpu_init(&cpu);
-
     Bus bus = {0};
+    PPU ppu = {0};
+    Timer timer = {0};
+
+    cpu_init(&cpu);
+    ppu_init(&ppu);
+    timer_init(&timer);
 
     return 0;
 }
